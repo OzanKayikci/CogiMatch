14defdf240190f586e09795d933fafd9744356d4
ozankayikci <ozankayikci0@gmail.com>
2023-10-26 10:21:24 +0300

    Some bug fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,19 +14,20 @@ android {
         applicationId = "com.laivinieks.cogimatch"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            isDebuggable = false
         }
     }
     compileOptions {
